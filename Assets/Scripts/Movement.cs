cc9f2c0f5470863622ac2ac40e466c266af10fca
Turner Millsaps <turnermillsaps.plp@gmail.com>
2024-02-22 14:20:37 -0700

    First commit and adding sandbox env

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            // Add relative force to rigid body's relative position, 1 on x, 1 on y and 1 on z
+            // Shorthand for up would be Vector3.up
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // Freeze the rotation so the physics engine doesn't take over when hitting an object
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // Unfreeze the rotation so physics system can take over again
+    }
+}
