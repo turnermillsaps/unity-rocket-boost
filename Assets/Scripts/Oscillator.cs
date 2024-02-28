06c00bdb5c934d1ed89e41cf0a3b8bb5c61a1e73
Turner Millsaps <turnermillsaps.plp@gmail.com>
2024-02-28 14:36:52 -0700

    Added oscillation to one of the obstacles

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
+    float movementFactor;
+
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 3.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,15 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; }
+        
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2; // recalculated to go from 0 to 1 so its cleaner
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
